9f4b6dd941515d88aee93a018145b0da77eb4148
river06 <weich06@gmail.com>
2015-01-30 15:25:33 -0500

    remove_duplicate_sorted_array_II

---
+++ remove_duplicate_sorted_array_II/Solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int removeDuplicates(int[] A) {
+		if(A.length == 0) return 0;
+		if(A.length == 1) return 1;
+		
+        int p1 = 0;
+		int p2 = 0;
+		int num_du = 0;
+		int if_du = 0;
+
+		while(p2<A.length-1) {
+			
+			if(A[p2] == A[p2+1] && if_du == 1) {
+				num_du ++;
+				p2++;
+			} else  {
+				if(A[p2] == A[p2+1]) {
+					if_du = 1;
+				} else {
+					if_du = 0;
+				}
+				
+				A[p1] = A[p2];
+				p1++;
+				p2++;
+			}
+		}
+
+		// last element
+
+		A[p1] = A[A.length-1];
+
+
+		return A.length - num_du;
+    }
+}
