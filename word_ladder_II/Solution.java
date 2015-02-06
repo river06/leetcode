5c0a706b65dd20a3450a7ddd5aacacfcd75d1a29
Hongchuan <weihc06@gmail.com>
2015-02-06 17:00:30 -0500

    temp

---
+++ word_ladder_II/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public List<List<String>> findLadders(String start, String end, Set<String> dict) {
+        // this method uses BFS
+		// this method uses container to store the paths
+
+		Queue<String> queue = new Queue<String>();
+		queue.add(start); // put the first word in the queue
+		
+    }
+}
