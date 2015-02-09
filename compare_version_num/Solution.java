4ed9f1ae7ec9df73e9e3293afd14aacc7526b02e
Hongchuan <weihc06@gmail.com>
2015-02-09 11:37:37 -0500

    compare_version_num/

--- compare_version_num/Solution.java
+++ compare_version_num/Solution.java
@@ -1,31 +1,36 @@
 public class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split(".");
-		String[] v2 = version2.split(".");
+        String[] v1 = version1.split("\\.");
+		String[] v2 = version2.split("\\.");
 
-		System.out.println(v1);
+//		System.out.println(v1);
 
 		int n = Math.min(v1.length, v2.length);
-		System.out.println(n);
+		int m = Math.max(v1.length, v2.length);
+//		System.out.println(n);
 		
-		for(int i=0; i<n; i++) {
-			int num1 = Integer.parseInt(v1[i]);
-			int num2 = Integer.parseInt(v2[i]);
+		for(int i=0; i<m; i++) {
+			int num1 = 0;
+			int num2 = 0;
+			if(i <n ) {
+				num1 = Integer.parseInt(v1[i]);
+				num2 = Integer.parseInt(v2[i]);
+			} else {
+				if( i<v1.length ) {
+					num1 = Integer.parseInt(v1[i]);
+				} else if( i<v2.length ) {
+					num2 = Integer.parseInt(v2[i]);
+				}
+			}
 
-			System.out.println(num1);
-			System.out.println(num2);
+//			System.out.println(num1);
+//			System.out.println(num2);
 			if( num1 > num2 ) {
 				return 1;
 			} else if (num1 <num2) {
 				return -1;
 			}
 		}
-		if(v1.length == v2.length ){
-			return 0;
-		} else if( v1.length>v2.length) {
-			return 1;
-		} else {
-			return -1;
-		}
+		return 0;
     }
 }
--- compare_version_num/Test.java
+++ compare_version_num/Test.java
@@ -1,8 +1,8 @@
 public class Test {
 	public static void main(String[] args) {
 		Solution solver = new Solution();
-		String v1 = "0.1";
-		String v2 = "0.2";
+		String v1 = "1.0";
+		String v2 = "1";
 		int ret = solver.compareVersion(v1, v2);
 		System.out.println(ret);
 	}
